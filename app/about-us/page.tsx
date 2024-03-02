04c1a58069e85f5d9b585448f393c2d72c6bc19a
rlawlsdn263 <rlawlsdn263@naver.com>
2024-03-02 20:37:39 +0900

    [#2.1] 컴포넌트 보관용 폴더
    `page` 파일이 없기 때문에, `components` 폴더는 URL로 인식되지 않는다. 고로, 해당 폴더는 컴포넌트를 지닌 평범한 폴더에 불과하다.
    
    그리고 이렇게 만들어둔 컴포넌트들을 다른 `page` 파일에 사용하는 것도 가능하다.

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,3 +1,10 @@
+import Avatar from "./components/avatar";
+
 export default function AboutUs() {
-  return <h1>About Us!</h1>;
+  return (
+    <>
+      <h1>About Us!</h1>;
+      <Avatar />
+    </>
+  );
 }
