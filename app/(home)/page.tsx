d612a0878d8e9b6f197d161da872739f29d118a1
rlawlsdn263 <rlawlsdn263@naver.com>
2024-03-31 15:57:51 +0900

    [#3.1] 클라이언트 사이드 통신
    클라이언트 컴포넌트에서 데이터 통신을 수행하고, UI를 렌더링하기 위해서는 `useState`, `useEffect`가 필요하다.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,9 +3,23 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello NextJS14!</h1>;
-    </div>
-  );
+  // 1. movies 데이터를 담을 state 생성
+  const [movies, setMovies] = useState();
+
+  // 2. movies 데이터를 호출하고, setMovies에 업데이트할 함수 생성
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+  };
+
+  // 3. useEffect를 사용해 getMovies 함수를 실행시킴
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  // 4. movies state를 렌더링함
+  return <div>{JSON.stringify(movies)}</div>;
 }
