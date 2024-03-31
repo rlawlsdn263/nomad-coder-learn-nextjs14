71cdb847bce1223a376cac76c031c57700db5dc2
rlawlsdn263 <rlawlsdn263@naver.com>
2024-03-31 16:00:51 +0900

    [#3.1] 로딩 UI 만들기
    데이터를 호출하고 렌더링하는 동안 사용자한테 보여줄 로딩 UI를 만들었다.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,23 +3,21 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  // 1. movies 데이터를 담을 state 생성
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // 2. movies 데이터를 호출하고, setMovies에 업데이트할 함수 생성
   const getMovies = async () => {
     const response = await fetch(
       "https://nomad-movies.nomadcoders.workers.dev/movies"
     );
     const json = await response.json();
     setMovies(json);
+    setIsLoading(false);
   };
 
-  // 3. useEffect를 사용해 getMovies 함수를 실행시킴
   useEffect(() => {
     getMovies();
   }, []);
 
-  // 4. movies state를 렌더링함
-  return <div>{JSON.stringify(movies)}</div>;
+  return <div>{isLoading ? "로딩 중..." : JSON.stringify(movies)}</div>;
 }
