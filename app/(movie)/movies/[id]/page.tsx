120bee9a2b9471a540f7457694b311e259b63e0f
rlawlsdn263 <rlawlsdn263@naver.com>
2024-04-03 15:09:51 +0900

    [#3.4] Promise는 순차적으로 해결된다
    `/movies/[id]`에서 통신을 여러 개를 수행할 경우, 통신은 순차적으로 진행이 된다. 그렇기 때문에 앞의 통신이 오래 걸리면, 뒤에 통신은 앞이 끝날 때까지 기다려야 한다.
    
    로딩 UI를 제공하더라도 순차적인 통신은 상당히 치명적이다.

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -1,9 +1,18 @@
 import { API_URL } from "../../../(home)/page";
 
 async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  console.log(`Fetching Movie: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
   const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching Videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
 
@@ -12,7 +21,10 @@ export default async function MovieDetail({
 }: {
   params: { id: string };
 }) {
+  console.log("Start Fetching!");
   const movie = await getMovie(id);
+  const video = await getVideos(id);
+  console.log("End Fetching!");
 
   return <div>{movie.title}</div>;
 }
